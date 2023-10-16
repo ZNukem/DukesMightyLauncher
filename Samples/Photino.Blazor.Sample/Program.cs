f30d36c5a478612a3f327a1ee978d7e2e270ab82
ZLegendaryGamer <16799827+ZNukem@users.noreply.github.com>
2023-10-16 12:08:57 -0500

    uploading files

--- Samples/Photino.Blazor.Sample/Program.cs
+++ Samples/Photino.Blazor.Sample/Program.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Photino.Blazor;
 
-namespace Photino.Blazor.Sample
+namespace DML
 {
     class Program
     {
@@ -23,7 +23,7 @@ namespace Photino.Blazor.Sample
             // customize window
             app.MainWindow
                 .SetIconFile("favicon.ico")
-                .SetTitle("Photino Blazor Sample");
+                .SetTitle("Dukes Mighty Launcher");
 
             AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
             {
