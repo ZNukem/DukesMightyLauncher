d0a8aea803377096761bd665ce095aafcd37160c
ZLegendaryGamer <16799827+ZNukem@users.noreply.github.com>
2023-10-16 12:04:09 -0500

    Uploading new rewrite version of DML.
    Going to rework the program as blazor app to allow launching and stuff to be a thing. ; )

---
+++ Samples/Photino.Blazor.Sample/classes/game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photino.Blazor.Sample.classes
+{
+    internal class game
+    {
+    }
+}
